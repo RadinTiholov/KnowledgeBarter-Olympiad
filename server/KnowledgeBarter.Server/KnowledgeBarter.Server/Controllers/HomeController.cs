0197a4c4567beab20fd7968dda2379509264af35
RadinTiholov <radin.tiholov@mail.bg>
2022-12-26 13:12:44 +0200

    Initial commit

---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace KnowledgeBarter.Server.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        //[Authorize]
+        public async Task<IActionResult> Get()
+        {
+            return this.Ok("Works");
+        }
+    }
+}
---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Data/KnowledgeBarterDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnowledgeBarter.Server.Data
+{
+    public class KnowledgeBarterDbContext : IdentityDbContext
+    {
+        public KnowledgeBarterDbContext(DbContextOptions<KnowledgeBarterDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
