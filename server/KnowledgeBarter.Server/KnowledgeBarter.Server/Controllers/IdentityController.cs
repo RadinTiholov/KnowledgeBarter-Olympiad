4a2220a5b36bfbc5ddcb241825166c8b842e4c1f
RadinTiholov <radin.tiholov@mail.bg>
2022-12-27 19:15:31 +0200

    Add ClaimsPrincipalExtension

--- server/KnowledgeBarter.Server/KnowledgeBarter.Server/Controllers/IdentityController.cs
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Controllers/IdentityController.cs
@@ -64,7 +64,8 @@ namespace KnowledgeBarter.Server.Controllers
             {
                 Subject = new ClaimsIdentity(new[]
                 {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.NameIdentifier, user.UserName),
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Infrastructure/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace KnowledgeBarter.Server.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+                ?.Value;
+        }
+    }
+}
