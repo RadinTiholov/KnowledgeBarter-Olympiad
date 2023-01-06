cbfbe65b7f6c2cab86fcb44276b950043fcda0d2
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-01-06 19:07:11 +0200

    Create CommentController.cs

---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Controllers/CommentController.cs
@@ -0,0 +1,16 @@
+﻿using KnowledgeBarter.Server.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+
+namespace KnowledgeBarter.Server.Controllers
+{
+    [Authorize]
+    public class CommentController : ApiController
+    {
+        private readonly ICommentService commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+    }
+}
