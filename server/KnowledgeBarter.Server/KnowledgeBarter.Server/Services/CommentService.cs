a38afb7d79de87172a6a556bd9cf2dfce8fa504b
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-01-06 19:25:33 +0200

    Update CommentService.cs

--- server/KnowledgeBarter.Server/KnowledgeBarter.Server/Services/CommentService.cs
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Services/CommentService.cs
@@ -1,10 +1,20 @@
-﻿using KnowledgeBarter.Server.Services.Contracts;
+﻿using KnowledgeBarter.Server.Data.Common.Repositories;
+using KnowledgeBarter.Server.Data.Models;
+using KnowledgeBarter.Server.Models.Comments;
+using KnowledgeBarter.Server.Services.Contracts;
 
 namespace KnowledgeBarter.Server.Services
 {
     public class CommentService : ICommentService
     {
-        public Task<IEnumerable<T>> AllAsync<T>()
+        private readonly IRepository<Comment> commentRepository;
+
+        public CommentService(IRepository<Comment> commentRepository)
+        {
+            this.commentRepository = commentRepository;
+        }
+
+        public Task<IEnumerable<CommentInListResponseModel>> AllAsync()
         {
             throw new NotImplementedException();
         }
