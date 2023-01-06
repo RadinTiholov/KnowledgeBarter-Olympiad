28914e36482fe03934280e4fca6ecb4dfee3f0da
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-01-06 19:25:20 +0200

    Update CommentController.cs

--- server/KnowledgeBarter.Server/KnowledgeBarter.Server/Controllers/CommentController.cs
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Controllers/CommentController.cs
@@ -1,5 +1,8 @@
-﻿using KnowledgeBarter.Server.Services.Contracts;
+﻿using KnowledgeBarter.Server.Models.Comments;
+using KnowledgeBarter.Server.Models.Lesson;
+using KnowledgeBarter.Server.Services.Contracts;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KnowledgeBarter.Server.Controllers
 {
@@ -11,6 +14,15 @@ namespace KnowledgeBarter.Server.Controllers
         public CommentController(ICommentService commentService)
         {
             this.commentService = commentService;
+        }
+
+        [HttpGet]
+        [Route(nameof(All))]
+        public async Task<IEnumerable<CommentInListResponseModel>> All()
+        {
+            var all = await this.commentService.AllAsync();
+
+            return all;
         }
     }
 }
