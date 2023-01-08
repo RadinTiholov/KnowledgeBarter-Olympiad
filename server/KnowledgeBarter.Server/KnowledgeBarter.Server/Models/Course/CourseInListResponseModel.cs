2ec1e6594a157047e505ff2cc48dbef0dac7359a
RadinTiholov <radin.tiholov@mail.bg>
2023-01-08 21:10:19 +0200

    Update CourseInList Model

--- server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Course/CourseInListResponseModel.cs
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Course/CourseInListResponseModel.cs
@@ -1,12 +1,38 @@
 ﻿namespace KnowledgeBarter.Server.Models.Course
 {
+    using AutoMapper;
     using KnowledgeBarter.Server.Data.Models;
     using KnowledgeBarter.Server.Services.Mapping;
 
-    public class CourseInListResponseModel : IMapFrom<Course>
+    public class CourseInListResponseModel : IMapFrom<Course>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
-        public int Title { get; set; }
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Thumbnail { get; set; } = null!;
+
+        public int Likes { get; set; }
+
+        public int Price { get; set; }
+
+        public string Owner { get; set; } = null!;
+
+        public int[] Lessons { get; set; } = null!;
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Course, CourseInListResponseModel>()
+                .ForMember(x => x.Thumbnail, opt =>
+                    opt.MapFrom(i => i.Image.Url))
+                .ForMember(x => x.Owner, opt =>
+                    opt.MapFrom(i => i.OwnerId))
+                .ForMember(x => x.Likes, opt =>
+                    opt.MapFrom(i => i.Likes.Count()))
+                .ForMember(x => x.Lessons, opt =>
+                    opt.MapFrom(i => i.Lessons.Select(x => x.Id).ToArray()));
+        }
     }
 }
