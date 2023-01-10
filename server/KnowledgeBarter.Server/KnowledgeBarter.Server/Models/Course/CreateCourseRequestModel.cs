501ce30dc88e4916d0eac90720393b56b4eeb4ec
RadinTiholov <radin.tiholov@mail.bg>
2023-01-10 19:13:05 +0200

    Create the request model for course creation

---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Course/CreateCourseRequestModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static KnowledgeBarter.Server.Data.Common.DataValidation.Course;
+
+
+namespace KnowledgeBarter.Server.Models.Course
+{
+    public class CreateCourseRequestModel
+    {
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Url]
+        public string Image { get; set; } = null!;
+
+        [Required]
+        [MinLength(3)]
+        public string[] Lessons { get; set; }
+    }
+}
