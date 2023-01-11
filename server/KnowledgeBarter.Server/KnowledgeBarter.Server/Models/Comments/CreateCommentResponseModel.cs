eb3b9a4b048d3495e9b75922711b2ced0fc8deda
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-01-11 23:04:45 +0200

    Create CreateCommentResponseModel.cs

---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Comments/CreateCommentResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using KnowledgeBarter.Server.Data.Models;
+using KnowledgeBarter.Server.Services.Mapping;
+
+namespace KnowledgeBarter.Server.Models.Comments
+{
+    public class CreateCommentResponseModel : IMapFrom<Comment>
+    {
+        public string Text { get; set; }
+    }
+}
