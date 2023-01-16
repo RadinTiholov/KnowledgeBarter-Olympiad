3f6bb141090063eb98800f9cd63c1b741c3260c2
RadinTiholov <radin.tiholov@mail.bg>
2023-01-16 15:28:50 +0200

    Fix logout functionality

--- client/knowledge-barter/src/components/Logout/Logout.js
+++ client/knowledge-barter/src/components/Logout/Logout.js
@@ -1,21 +1,14 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
-import * as authService from "../../services/authService" 
 
 export const Logout = () => {
     const navigate = useNavigate();
-    const {auth,userLogout} = useContext(AuthContext);
+    const { userLogout } = useContext(AuthContext);
     useEffect(() => {
-        authService.logout({accessToken: auth.accessToken})
-            .then(res => {
-                userLogout();
-                localStorage.clear();
-                navigate('/');
-            })
-            .catch(() => {
-                navigate('/')
-            })
+        userLogout();
+        localStorage.clear();
+        navigate('/');
     }, [])
 
     return null;
--- client/knowledge-barter/src/services/authService.js
+++ client/knowledge-barter/src/services/authService.js
@@ -8,15 +8,11 @@ const login = (data) => {
 const register = (data) => {
     return request.post(`${baseUrl}/identity/register`, data)
 }
-const logout = () => {
-    return request.get(`${baseUrl}/auth/logout`)
-}
 const getDetails = (id) => {
     return request.get(`${baseUrl}/identity/profile` + id)
 }
 export{
     login,
     register,
-    logout,
     getDetails
 }
