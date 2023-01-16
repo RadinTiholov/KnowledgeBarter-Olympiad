e73cbabbc0d432f3415f5608df68c5bb2083cb04
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-01-16 15:05:46 +0200

    Update CreateCommentResponseModel.cs

--- server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Comments/CreateCommentResponseModel.cs
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Comments/CreateCommentResponseModel.cs
@@ -1,10 +1,27 @@
-﻿using KnowledgeBarter.Server.Data.Models;
+﻿using AutoMapper;
+using KnowledgeBarter.Server.Data.Models;
+using KnowledgeBarter.Server.Models.Course;
 using KnowledgeBarter.Server.Services.Mapping;
 
 namespace KnowledgeBarter.Server.Models.Comments
 {
-    public class CreateCommentResponseModel : IMapFrom<Comment>
+    public class CreateCommentResponseModel : IMapFrom<Comment>, IHaveCustomMappings
     {
+        public int Id { get; set; }
+
         public string Text { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string ProfilePicture { get; set; } = null!;
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Comment, CreateCommentResponseModel>()
+                .ForMember(c => c.UserName, opt =>
+                    opt.MapFrom(x => x.Owner.UserName))
+                 .ForMember(c => c.ProfilePicture, opt =>
+                    opt.MapFrom(x => x.Owner.Image.Url));
+        }
     }
 }
