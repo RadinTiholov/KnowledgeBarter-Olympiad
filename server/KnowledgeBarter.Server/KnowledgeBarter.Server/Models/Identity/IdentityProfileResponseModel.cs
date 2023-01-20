b883c826a3526afe9985c124cb0f850b37a6d549
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-01-20 21:33:18 +0200

    Create IdentityProfileResponseModel.cs

---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Identity/IdentityProfileResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using KnowledgeBarter.Server.Data.Models;
+using KnowledgeBarter.Server.Services.Mapping;
+
+namespace KnowledgeBarter.Server.Models.Identity
+{
+    public class IdentityProfileResponseModel
+    {
+        public string Username { get; set; } = null!;
+
+        public string ProfilePicture { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public Image Image { get; set; } = null!;
+    }
+}
