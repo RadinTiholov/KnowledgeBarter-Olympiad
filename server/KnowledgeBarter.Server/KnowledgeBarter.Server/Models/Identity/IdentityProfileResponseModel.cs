56ac57effde05e88a0fc98199f2f2061fd934f78
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-01-20 21:43:34 +0200

    Update IdentityProfileResponseModel.cs

--- server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Identity/IdentityProfileResponseModel.cs
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Identity/IdentityProfileResponseModel.cs
@@ -1,16 +1,26 @@
-﻿using KnowledgeBarter.Server.Data.Models;
+﻿using AutoMapper;
+using KnowledgeBarter.Server.Data.Models;
+using KnowledgeBarter.Server.Models.Comments;
+using KnowledgeBarter.Server.Models.Lesson;
 using KnowledgeBarter.Server.Services.Mapping;
 
 namespace KnowledgeBarter.Server.Models.Identity
 {
-    public class IdentityProfileResponseModel
+    public class IdentityProfileResponseModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
     {
         public string Username { get; set; } = null!;
 
-        public string ProfilePicture { get; set; } = null!;
-
         public string Email { get; set; } = null!;
 
-        public Image Image { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+
+        public int KBPoints { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ApplicationUser, IdentityProfileResponseModel>()
+                .ForMember(x => x.ImageUrl, opt =>
+                    opt.MapFrom(a => a.Image.Url));
+        }
     }
 }
