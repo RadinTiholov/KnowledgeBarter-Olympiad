0d79f2581d2a67942a9eb8665e4605def4085c7c
RadinTiholov <radin.tiholov@mail.bg>
2023-01-29 11:52:33 +0200

    Add Trained ML model for binary classification

---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/ML/ReviewPredictionModel.cs
@@ -0,0 +1,33 @@
+﻿namespace ML
+{
+    using Microsoft.ML;
+
+    public class ReviewPredictionModel
+    {
+        private static Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictionEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(CreatePredictionEngine);
+
+        public static ModelOutput Predict(ModelInput input)
+        {
+            ModelOutput result = PredictionEngine.Value.Predict(input);
+            return result;
+        }
+
+        public static PredictionEngine<ModelInput, ModelOutput> CreatePredictionEngine()
+        {
+            MLContext mlContext = new MLContext();
+
+            //string modelPath = @"C:\Users\radit\AppData\Local\Temp\MLVSTools\AutoMlTestML\AutoMlTestML.Model\MLModel.zip";
+
+            //For unit testing
+            string modelPath = Directory.GetCurrentDirectory() + "\\MLModel.zip";
+            if (!modelPath.Contains("Test"))
+            {
+                modelPath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\ML.Models\\MLModel.zip");
+            }
+            ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            return predEngine;
+        }
+    }
+}
