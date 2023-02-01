14d65243e9dcffd87e975d28d4d26de809352481
RadinTiholov <radin.tiholov@mail.bg>
2023-02-01 19:27:17 +0200

    Remove the unnecessary props from the lesson model

--- server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Lesson/LessonInListResponseModel.cs
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Lesson/LessonInListResponseModel.cs
@@ -3,7 +3,6 @@
     using KnowledgeBarter.Server.Services.Mapping;
     using KnowledgeBarter.Server.Data.Models;
     using AutoMapper;
-    using KnowledgeBarter.Server.Models.Comments;
 
     public class LessonInListResponseModel : IMapFrom<Lesson>, IHaveCustomMappings
     {
@@ -13,46 +12,14 @@
 
         public string Description { get; set; } = null!;
 
-        public string Article { get; set; } = null!;
-
         public string Thumbnail { get; set; } = null!;
 
-        public string Video { get; set; } = null!;
-
-        public string Resources { get; set; } = null!;
-
-        public int Likes { get; set; }
-
-        public int Price { get; set; }
-
-        public int Views { get; set; }
-
-        public string Owner { get; set; } = null!;
-
-        public string[] Tags { get; set; } = null!;
-
-        public IEnumerable<CommentInListResponseModel> Comments { get; set; } = null!;
 
         public void CreateMappings(IProfileExpression configuration)
         {
             configuration.CreateMap<Lesson, LessonInListResponseModel>()
                 .ForMember(x => x.Thumbnail, opt =>
-                    opt.MapFrom(i => i.Image.Url))
-                .ForMember(x => x.Owner, opt =>
-                    opt.MapFrom(i => i.OwnerId))
-                .ForMember(x => x.Likes, opt =>
-                    opt.MapFrom(i => i.Likes.Count()))
-                .ForMember(x => x.Tags, opt =>
-                    opt.MapFrom(i => i.Tags.Select(x => x.Text).ToArray()))
-                .ForMember(x => x.Comments, opt =>
-                   opt.MapFrom(i => i.Comments.Select(x => new CommentInListResponseModel()
-                   {
-                       Id = x.Id,
-                       Text = x.Text,
-                       LessonId = x.LessonId,
-                       UserName = x.Owner.UserName,
-                       ProfilePicture = x.Owner.Image.Url
-                   })));
+                    opt.MapFrom(i => i.Image.Url));
         }
     }
 }
