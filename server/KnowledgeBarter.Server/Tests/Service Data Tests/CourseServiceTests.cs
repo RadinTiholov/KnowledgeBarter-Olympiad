a7a0212276078bece073db52d35f9bd79f4f0f63
RadinTiholov <radin.tiholov@mail.bg>
2023-02-09 15:16:04 +0200

    Write tests for course service

--- server/KnowledgeBarter.Server/Tests/Service Data Tests/CourseServiceTests.cs
+++ server/KnowledgeBarter.Server/Tests/Service Data Tests/CourseServiceTests.cs
@@ -1,20 +1,25 @@
-﻿using KnowledgeBarter.Server;
-using KnowledgeBarter.Server.Data;
+﻿using KnowledgeBarter.Server.Data;
 using KnowledgeBarter.Server.Data.Common.Repositories;
 using KnowledgeBarter.Server.Data.Models;
 using KnowledgeBarter.Server.Data.Repositories;
+using KnowledgeBarter.Server.Models.Course;
+using KnowledgeBarter.Server.Models.Course.Base;
 using KnowledgeBarter.Server.Services;
 using KnowledgeBarter.Server.Services.Contracts;
+using KnowledgeBarter.Server.Services.Mapping;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 using Moq;
+using System.Reflection;
 using Xunit;
 
 namespace Tests.Service_Data_Tests
 {
     public class CourseServiceTests
     {
+        private IFormFile formFile;
         private IDeletableEntityRepository<Course> courseRepository;
         private IRepository<ApplicationUser> applicationUserRepository;
         private IRepository<Lesson> lessonRepository;
@@ -48,7 +53,7 @@ namespace Tests.Service_Data_Tests
             this.imageRepository = new EfRepository<Image>(this.knowledgeBarterDbContext);
             this.likeRepository = new EfRepository<Like>(this.knowledgeBarterDbContext);
 
-
+            this.formFile = this.CreateFakeFormFile();
             var mockCloudinaryService = new Mock<ICloudinaryService>();
             mockCloudinaryService.Setup(x => x.UploadAsync(It.IsAny<IFormFile>(), It.IsAny<string>()))
                 .ReturnsAsync(() =>
@@ -121,9 +126,369 @@ namespace Tests.Service_Data_Tests
         {
             await this.SeedData();
 
-            var result = await this.courseService.IsBoughtOrOwnerAsync(1, "userId2");
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.IsBoughtOrOwnerAsync(1, "userId32"); });
+        }
 
-            Assert.False(result);
+        [Fact]
+        public async Task AllAsyncShouldWorkCorrectly()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(CourseInListResponseModel).GetTypeInfo().Assembly);
+
+            await this.SeedData();
+
+            var courses = await this.courseService.AllAsync();
+
+            Assert.Single(courses);
+        }
+
+        [Fact]
+        public async Task HighestAsyncShouldWorkCorrectly()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(CourseInListResponseModel).GetTypeInfo().Assembly);
+
+            await this.SeedData();
+
+            var courses = await this.courseService.AllAsync();
+
+            Assert.Single(courses);
+        }
+
+        [Fact]
+        public async Task GetOneAsyncShouldThrowExWhenNotFound()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(BaseCourseDetailsResponseModel).GetTypeInfo().Assembly);
+            await this.SeedData();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.GetOneAsync<BaseCourseDetailsResponseModel>(2); });
+        }
+
+        [Fact]
+        public async Task GetOneAsyncShouldWorkCorrectly()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(BaseCourseDetailsResponseModel).GetTypeInfo().Assembly);
+            await this.SeedData();
+
+            var course = await this.courseService.GetOneAsync<BaseCourseDetailsResponseModel>(1);
+
+            Assert.True(course != null);
+            Assert.Equal("Test", course.Title);
+        }
+
+        [Fact]
+        public async Task GetOneAsyncShouldWorkCorrectlyWhenBought()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(BoughtCourseDetailsResponseModel).GetTypeInfo().Assembly);
+            await this.SeedData();
+
+            var course = await this.courseService.GetOneAsync<BoughtCourseDetailsResponseModel>(1);
+
+            Assert.True(course != null);
+            Assert.Equal("Test", course.Title);
+        }
+
+        [Fact]
+        public async Task DeleteAsyncShouldWorkCorrectly()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(CourseInListResponseModel).GetTypeInfo().Assembly);
+            await this.SeedData();
+
+            await this.courseService.DeleteAsync(1, "userId1");
+
+            var courses = await this.courseService.AllAsync();
+
+            Assert.Empty(courses);
+        }
+
+        [Fact]
+        public async Task DeleteAsyncShouldThrowExWhenNotFound()
+        {
+            await this.SeedData();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.DeleteAsync(12, "userId1"); });
+        }
+
+        [Fact]
+        public async Task DeleteAsyncShouldThrowExWhenNotOwner()
+        {
+            await this.SeedData();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.DeleteAsync(1, "userId2"); });
+        }
+
+        [Fact]
+        public async Task LikeAsyncShouldWorkCorrectly()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(BoughtCourseDetailsResponseModel).GetTypeInfo().Assembly);
+            await this.SeedData();
+
+            await this.courseService.LikeAsync(1, "userId2");
+
+            var course = await this.courseService.GetOneAsync<BoughtCourseDetailsResponseModel>(1);
+
+            Assert.Equal(2, course.Likes);
+        }
+
+        [Fact]
+        public async Task LikeAsyncShouldThrowExWhenOwner()
+        {
+            await this.SeedData();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.LikeAsync(1, "userId1"); });
+        }
+
+        [Fact]
+        public async Task LikeAsyncShouldThrowExWhenNotFound()
+        {
+            await this.SeedData();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.LikeAsync(4, "userId3"); });
+        }
+
+        [Fact]
+        public async Task BuyAsyncShouldWorkCorrectly()
+        {
+            await this.SeedData();
+
+            await this.courseService.BuyAsync(1, "userId2");
+
+            var user = await this.knowledgeBarterDbContext
+                .Users
+                .Include(x => x.BoughtCourses)
+                .Where(x => x.Id == "userId2")
+                .FirstAsync();
+
+            Assert.Equal(19500, user.KBPoints);
+            Assert.Equal(1, user.BoughtCourses.Count);
+        }
+
+        [Fact]
+        public async Task BuyAsyncShouldThrowExWhenNotFound()
+        {
+            await this.SeedData();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.BuyAsync(3, "userId1"); });
+        }
+
+        [Fact]
+        public async Task BuyAsyncShouldThrowExWhenOwner()
+        {
+            await this.SeedData();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.BuyAsync(1, "userId1"); });
+        }
+
+        [Fact]
+        public async Task BuyAsyncShouldThrowExWhenNotEnoghtPoints()
+        {
+            await this.SeedData();
+            var user = await this.knowledgeBarterDbContext.Users.Where(x => x.Id == "userId2").FirstAsync();
+
+            user.KBPoints = 0;
+
+            this.knowledgeBarterDbContext.Users.Update(user);
+            await this.knowledgeBarterDbContext.SaveChangesAsync();
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.BuyAsync(1, "userId2"); });
+        }
+
+        [Fact]
+        public async Task CreateAsyncShouldWorkCorrectly()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(CreateCourseResponseModel).GetTypeInfo().Assembly);
+            await this.SeedData();
+
+            await this.SeedLessons();
+
+
+            var model = new CreateCourseRequestModel()
+            {
+                Title = "Test11",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Image = this.formFile,
+                Lessons = new int[] { 1, 2, 3, 4, 5, 6 }
+            };
+
+            var response = await this.courseService.CreateAsync(model, "userId1");
+
+            var courses = await this.courseService.AllAsync();
+
+            Assert.NotEmpty(courses);
+            Assert.Equal(2, courses.Count());
+        }
+
+        [Fact]
+        public async Task CreateAsyncShouldThrowExWhenNotEnough()
+        {
+            await this.SeedData();
+
+            await this.SeedLessons();
+
+            var model = new CreateCourseRequestModel()
+            {
+                Title = "Test11",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Image = this.formFile,
+                Lessons = new int[] { 1, 2, 3, 4 }
+            };
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.CreateAsync(model, "userId1"); });
+        }
+
+        [Fact]
+        public async Task CreateAsyncShouldThrowExWhenDistinct()
+        {
+            await this.SeedData();
+
+            await this.SeedLessons();
+
+            var model = new CreateCourseRequestModel()
+            {
+                Title = "Test11",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Image = this.formFile,
+                Lessons = new int[] { 1, 2, 3, 4, 4, 4, 4 }
+            };
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.CreateAsync(model, "userId1"); });
+        }
+
+        [Fact]
+        public async Task CreateAsyncShouldThrowExWhenFromDiffUser()
+        {
+            await this.SeedData();
+
+            await this.SeedLessons();
+
+            var model = new CreateCourseRequestModel()
+            {
+                Title = "Test11",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Image = this.formFile,
+                Lessons = new int[] { 1, 2, 3, 4, 5, 6, 7 }
+            };
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => { await this.courseService.CreateAsync(model, "userId1"); });
+        }
+
+        private async Task SeedLessons()
+        {
+            var image = new Image()
+            {
+                Id = 2,
+                Url = "aaaaaaaaaaa",
+            };
+
+            var applicationUser = await this.knowledgeBarterDbContext.Users.Where(x => x.Id == "userId1").FirstAsync();
+            var applicationUser2 = await this.knowledgeBarterDbContext.Users.Where(x => x.Id == "userId2").FirstAsync();
+            var lesson1 = new Lesson()
+            {
+                Id = 1,
+                Title = "aaaaaaaaaa",
+                Article = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                OwnerId = "userId",
+                Owner = applicationUser,
+                ImageId = 1,
+                Image = image,
+                Views = 2,
+                Video = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Price = 2,
+            };
+            var lesson2 = new Lesson()
+            {
+                Id = 2,
+                Title = "aaaaaaaaaa",
+                Article = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                OwnerId = "userId",
+                Owner = applicationUser,
+                ImageId = 1,
+                Image = image,
+                Views = 2,
+                Video = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Price = 2,
+            };
+            var lesson3 = new Lesson()
+            {
+                Id = 3,
+                Title = "aaaaaaaaaa",
+                Article = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                OwnerId = "userId",
+                Owner = applicationUser,
+                ImageId = 1,
+                Image = image,
+                Views = 2,
+                Video = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Price = 2,
+            };
+            var lesson4 = new Lesson()
+            {
+                Id = 4,
+                Title = "aaaaaaaaaa",
+                Article = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                OwnerId = "userId",
+                Owner = applicationUser,
+                ImageId = 1,
+                Image = image,
+                Views = 2,
+                Video = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Price = 2,
+            };
+            var lesson5 = new Lesson()
+            {
+                Id = 5,
+                Title = "aaaaaaaaaa",
+                Article = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                OwnerId = "userId",
+                Owner = applicationUser,
+                ImageId = 1,
+                Image = image,
+                Views = 2,
+                Video = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Price = 2,
+            };
+            var lesson6 = new Lesson()
+            {
+                Id = 6,
+                Title = "aaaaaaaaaa",
+                Article = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                OwnerId = "userId",
+                Owner = applicationUser,
+                ImageId = 1,
+                Image = image,
+                Views = 2,
+                Video = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Price = 2,
+            };
+            var lesson7 = new Lesson()
+            {
+                Id = 7,
+                Title = "aaaaaaaaaa",
+                Article = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                OwnerId = "userId2",
+                Owner = applicationUser2,
+                ImageId = 1,
+                Image = image,
+                Views = 2,
+                Video = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Price = 2,
+            };
+
+
+            await this.knowledgeBarterDbContext.Lessons.AddAsync(lesson1);
+            await this.knowledgeBarterDbContext.Lessons.AddAsync(lesson2);
+            await this.knowledgeBarterDbContext.Lessons.AddAsync(lesson3);
+            await this.knowledgeBarterDbContext.Lessons.AddAsync(lesson4);
+            await this.knowledgeBarterDbContext.Lessons.AddAsync(lesson5);
+            await this.knowledgeBarterDbContext.Lessons.AddAsync(lesson6);
+            await this.knowledgeBarterDbContext.Lessons.AddAsync(lesson7);
+
+            await this.knowledgeBarterDbContext.SaveChangesAsync();
         }
 
         public async Task SeedData()
@@ -146,7 +511,7 @@ namespace Tests.Service_Data_Tests
             var applicationUser2 = new ApplicationUser()
             {
                 Id = "userId2",
-                KBPoints = 0,
+                KBPoints = 20000,
                 UserName = "Test2",
                 Email = "TestEmail2",
                 Image = image,
@@ -163,12 +528,32 @@ namespace Tests.Service_Data_Tests
                 Owner = applicationUser,
                 OwnerId = "userId1",
             };
+            var like = new Like()
+            {
+                Owner = applicationUser,
+                OwnerId = "userId1",
+                Course = course,
+                CourseId = course.Id,
+            };
+            course.Likes.Add(like);
 
             await this.knowledgeBarterDbContext.Users.AddAsync(applicationUser);
             await this.knowledgeBarterDbContext.Users.AddAsync(applicationUser2);
             await this.knowledgeBarterDbContext.Courses.AddAsync(course);
 
             await this.knowledgeBarterDbContext.SaveChangesAsync();
+        }
+        private IFormFile CreateFakeFormFile()
+        {
+            var content = "Hello World from a Fake File";
+            var fileName = "test";
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            writer.Flush();
+            stream.Position = 0;
+
+            return new FormFile(stream, 0, stream.Length, "id_from_form", fileName);
         }
     }
 }
