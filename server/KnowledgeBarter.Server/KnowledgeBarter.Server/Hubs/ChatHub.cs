b2cd52f1006cfd3e3c697667b42af5cbd46fe724
RadinTiholov <radin.tiholov@mail.bg>
2023-03-09 20:57:16 +0200

    Create ChatHub

---
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace KnowledgeBarter.Server.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+
+        public void Subscribe(string connectionUsername)
+        {
+            string currentUserName = this.Context.User.Identity.Name;
+            this.Groups.AddToGroupAsync(this.Context.ConnectionId, currentUserName + connectionUsername);
+        }
+
+        public Task SendMessageToGroup(string receiver, string message)
+        {
+            var sender = this.Context.User.Identity.Name;
+
+            return this.Clients.Group(receiver + sender).SendAsync("ReceiveMessage", sender, message);
+        }
+    }
+}
