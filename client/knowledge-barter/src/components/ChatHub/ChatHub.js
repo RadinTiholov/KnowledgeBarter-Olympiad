216d53df6e97a18ddca275adf0cce9934f16f13e
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-03-11 23:06:32 +0200

    Update ChatHub component

--- client/knowledge-barter/src/components/ChatHub/ChatHub.js
+++ client/knowledge-barter/src/components/ChatHub/ChatHub.js
@@ -1,5 +1,131 @@
 export const ChatHub = () => {
     return (
-        <></>
+        <div className="row mx-3">
+            <h1>Messages</h1>
+            <div className="col-md-6 col-6 pt-5">
+                <div className="card card-display w-100 py-2">
+                    <div className="card comment-card card-display-details mx-5 my-2">
+                        <div className="row">
+                            <div className="col-1">
+                                <img
+                                    className="img-fluid rounded-circle profile-comment m-3"
+                                    src='https://avatars.githubusercontent.com/u/74610360?v=4'
+                                    alt="Lesson Pic"
+                                    style={{ objectFit: 'contain' }}
+                                />
+                            </div>
+                            <div className="col-11">
+                                <p className="mt-4">GOSHO</p>
+                            </div>
+                        </div>
+                        <div className="row mx-3">
+                        </div>
+                    </div>
+
+                    <div className="card comment-card card-display-details mx-5 my-2">
+                        <div className="row">
+                            <div className="col-1">
+                                <img
+                                    className="img-fluid rounded-circle profile-comment m-3"
+                                    src='https://avatars.githubusercontent.com/u/74610360?v=4'
+                                    alt="Lesson Pic"
+                                    style={{ objectFit: 'contain' }}
+                                />
+                            </div>
+                            <div className="col-11">
+                                <p className="mt-4">GOSHO</p>
+                            </div>
+                        </div>
+                        <div className="row mx-3">
+                        </div>
+                    </div>
+
+                    <div className="card comment-card card-display-details mx-5 my-2">
+                        <div className="row">
+                            <div className="col-1">
+                                <img
+                                    className="img-fluid rounded-circle profile-comment m-3"
+                                    src='https://avatars.githubusercontent.com/u/74610360?v=4'
+                                    alt="Lesson Pic"
+                                    style={{ objectFit: 'contain' }}
+                                />
+                            </div>
+                            <div className="col-11">
+                                <p className="mt-4">GOSHO</p>
+                            </div>
+                        </div>
+                        <div className="row mx-3">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Search */}
+            <div className="col-md-6 col-6 pt-5 ">
+
+                <div className="card py-2 card-display w-100">
+                    <div className="ps-3">
+                        <p className="d-inline pe-2">Search</p>
+                        <input type="text" />
+                    </div>
+                    <h2>Search for a user</h2>
+                    <div className="card comment-card card-display-details mx-5 my-2">
+                        <div className="row">
+                            <div className="col-1">
+                                <img
+                                    className="img-fluid rounded-circle profile-comment m-3"
+                                    src='https://avatars.githubusercontent.com/u/74610360?v=4'
+                                    alt="Lesson Pic"
+                                    style={{ objectFit: 'contain' }}
+                                />
+                            </div>
+                            <div className="col-11">
+                                <p className="mt-4">GOSHO</p>
+                            </div>
+                        </div>
+                        <div className="row mx-3">
+                        </div>
+                    </div>
+
+                    <div className="card comment-card card-display-details mx-5 my-2">
+                        <div className="row">
+                            <div className="col-1">
+                                <img
+                                    className="img-fluid rounded-circle profile-comment m-3"
+                                    src='https://avatars.githubusercontent.com/u/74610360?v=4'
+                                    alt="Lesson Pic"
+                                    style={{ objectFit: 'contain' }}
+                                />
+                            </div>
+                            <div className="col-11">
+                                <p className="mt-4">GOSHO</p>
+                            </div>
+                        </div>
+                        <div className="row mx-3">
+                        </div>
+                    </div>
+
+                    <div className="card comment-card card-display-details mx-5 my-2">
+                        <div className="row">
+                            <div className="col-1">
+                                <img
+                                    className="img-fluid rounded-circle profile-comment m-3"
+                                    src='https://avatars.githubusercontent.com/u/74610360?v=4'
+                                    alt="Lesson Pic"
+                                    style={{ objectFit: 'contain' }}
+                                />
+                            </div>
+                            <div className="col-11">
+                                <p className="mt-4">GOSHO</p>
+                            </div>
+                        </div>
+                        <div className="row mx-3">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
     )
 }
