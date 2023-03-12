1792cac3d743343ec67c0a4211f66bcb14ee4db3
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-03-12 12:55:08 +0200

    Update ChatHub

--- client/knowledge-barter/src/components/ChatHub/ChatHub.js
+++ client/knowledge-barter/src/components/ChatHub/ChatHub.js
@@ -1,3 +1,4 @@
+import "./ChatHub.css"
 import { useEffect } from 'react';
 import { useState } from 'react'
 import * as authService from '../../dataServices/authService';
@@ -32,10 +33,10 @@ export const ChatHub = () => {
     } 
 
     return (
-        <div className="row mx-3">
-            <div className="col-md-6 col-6 pt-5">
+        <div className="chat-hub row mx-3 flex-wrap">
+            <div className="hub-component col-md-6 col-6 pt-5">
                 <h1>Recent Messages</h1>
-                <div className="card card-display w-100 py-2">
+                <div className="card card-display w-100 py-2" style={{ height: "70vh" }}>
                     <div className="card comment-card card-display-details mx-5 my-2">
                         <div className="row">
                             <div className="col-1">
@@ -93,9 +94,9 @@ export const ChatHub = () => {
             </div>
 
             {/* Search */}
-            <div className="col-md-6 col-6 pt-5 ">
+            <div className="hub-component col-md-6 col-6 pt-5 pb-2 ">
                 <h1>Search for a user</h1>
-                <div className="card py-2 card-display w-100">
+                <div className="card py-2 card-display w-100" style={{ height: "70vh" }}>
                     <div className="p-3 mx-5">
                         <form className="d-flex justify-content-center" role="search" onSubmit={onSearch}>
                             <input
