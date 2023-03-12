31f18ea8a887049720ba55cb7880bf6015671d4c
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-03-12 12:14:43 +0200

    Chat hub lesson and course linking added

--- client/knowledge-barter/src/components/DetailsCourse/CourseDetailsBought/Comment/Comment.js
+++ client/knowledge-barter/src/components/DetailsCourse/CourseDetailsBought/Comment/Comment.js
@@ -1,17 +1,28 @@
+import { Link } from "react-router-dom"
+
 export const Comment = (props) => {
     return (
         <div className="card comment-card card-display-details mx-5 my-2">
             <div className="row">
                 <div className="col-1">
-                    <img
-                        className="img-fluid rounded-circle profile-comment m-3"
-                        src={props.profilePicture}
-                        alt="Lesson Pic"
-                        style={{ objectFit: 'contain' }}
-                    />
+                    <Link
+                        to={`/chat?receiver=${props.userName}`}
+                    >
+                        <img
+                            className="img-fluid rounded-circle profile-comment m-3"
+                            src={props.profilePicture}
+                            alt="Lesson Pic"
+                            style={{ objectFit: 'contain' }}
+                        />
+                    </Link>
                 </div>
                 <div className="col-11">
-                    <p className="mt-4">{props.userName}</p>
+                    <Link
+                        to={`/chat?receiver=${props.userName}`}
+                        style={{ textDecoration: "none" }}
+                    >
+                        <p className="mt-4">{props.userName}</p>
+                    </Link>
                 </div>
             </div>
             <div className="row mx-3">
