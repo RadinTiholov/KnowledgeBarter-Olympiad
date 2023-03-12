9dea6c55f08ae043b96a8d917ca3d30953a1a173
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-03-12 11:16:24 +0200

    Update ChatHub.js

--- client/knowledge-barter/src/components/ChatHub/ChatHub.js
+++ client/knowledge-barter/src/components/ChatHub/ChatHub.js
@@ -1,8 +1,22 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export const ChatHub = () => {
+    const [search, setSearch] = useState();
+    const navigate = useNavigate();
+    
+    const onChange = (e) => {
+        setSearch(e.target.value)
+    }
+    const onSearch = (e) => {
+        e.preventDefault();
+        navigate(`/lesson/all/${search}`)
+    } 
+
     return (
         <div className="row mx-3">
-            <h1>Messages</h1>
             <div className="col-md-6 col-6 pt-5">
+                <h1>Recent Messages</h1>
                 <div className="card card-display w-100 py-2">
                     <div className="card comment-card card-display-details mx-5 my-2">
                         <div className="row">
@@ -62,13 +76,28 @@ export const ChatHub = () => {
 
             {/* Search */}
             <div className="col-md-6 col-6 pt-5 ">
-
+                <h1>Search for a user</h1>
                 <div className="card py-2 card-display w-100">
-                    <div className="ps-3">
-                        <p className="d-inline pe-2">Search</p>
-                        <input type="text" />
+                    <div className="p-3 mx-5">
+                        <form className="d-flex justify-content-center" role="search" onSubmit={onSearch}>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="title"
+                                id="home-search"
+                                placeholder="Search"
+                                value={search}
+                                onChange={onChange}
+                            />
+                            <button
+                                className="btn btn-outline-warning"
+                                style={{ backgroundColor: "#636EA7" }}
+                                type="submit"
+                            >
+                                Search
+                            </button>
+                        </form>
                     </div>
-                    <h2>Search for a user</h2>
                     <div className="card comment-card card-display-details mx-5 my-2">
                         <div className="row">
                             <div className="col-1">
