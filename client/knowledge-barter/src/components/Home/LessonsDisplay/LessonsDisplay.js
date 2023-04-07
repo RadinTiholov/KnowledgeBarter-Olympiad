e688ed80df1ff347883118ccfd98382d97181978
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-04-07 18:48:20 +0300

    Fix carousel for lessons on home page

--- client/knowledge-barter/src/components/Home/LessonsDisplay/LessonsDisplay.js
+++ client/knowledge-barter/src/components/Home/LessonsDisplay/LessonsDisplay.js
@@ -12,76 +12,75 @@ export const LessonsDisplay = (props) => {
                     </div>
                 </div>
 
-                {props.isLoadingLessons || props.isLoadingCourses
-                    ? <BookSpinner />
-                    : <div style={{ paddingBottom: "2rem" }} className=''>
-                        <div className="row gy-5">
-                            {props.lessons ?
-                                props.lessons?.map(x => <Card key={x.id} route={props.route} {...x} />)
-                              : props.courses ? null : <p className='text-center'>No lessons yet!</p>}
-                            {props.courses ? props.courses?.map(x => <Card key={x.id} route={props.route} {...x} />) : props.lessons ? null : <p className='text-center'>No courses yet!</p>}
-                        </div>
-                    </div>
-                }
-
-                {props.isLoadingLessons || props.isLoadingCourses
-                    ? <BookSpinner />
-                    : <div style={{ paddingBottom: "2rem" }} className=''>
-                        <div className="row gy-5">
-                            <div className='big-screen'>
-                                {props.lessons
-                                    ?
-                                    <div
-                                        id="carouselExampleControls"
-                                        className="carousel slide"
-                                        data-bs-ride="carousel"
-                                    >
-                                        <div className="carousel-inner">
-                                            <div className="carousel-item active" >
-                                                <div style={{ display: 'flex' }}>
-                                                    <Card key={props.lessons[0].id} route={props.route} {...props.lessons[0]} />
-
-                                                    <Card key={props.lessons[0].id} route={props.route} {...props.lessons[0]} />
-                                                </div>
-                                            </div>
-                                            <div className="carousel-item">
-                                                <div style={{ display: 'flex' }}>
-
-                                                    {props.lessons?.map(x => <Card key={x.id} route={props.route} {...x} />)}
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <button
-                                            className="carousel-control-prev"
-                                            type="button"
-                                            data-bs-target="#carouselExampleControls"
-                                            data-bs-slide="prev"
-                                        >
-                                            <span className="carousel-control-prev-icon" aria-hidden="true" />
-                                            <span className="visually-hidden">Previous</span>
-                                        </button>
-                                        <button
-                                            className="carousel-control-next"
-                                            type="button"
-                                            data-bs-target="#carouselExampleControls"
-                                            data-bs-slide="next"
-                                        >
-                                            <span className="carousel-control-next-icon" aria-hidden="true" />
-                                            <span className="visually-hidden">Next</span>
-                                        </button>
-                                    </div>
-                                    //props.lessons?.map(x => <Card key={x.id} route={props.route} {...x} />)
+                <div className='small-screen'>
+                    {props.isLoadingLessons || props.isLoadingCourses
+                        ? <BookSpinner />
+                        : <div style={{ paddingBottom: "2rem" }} className=''>
+                            <div className="row gy-5">
+                                {props.lessons ?
+                                    props.lessons?.map(x => <Card key={x.id} route={props.route} {...x} />)
                                     : props.courses ? null : <p className='text-center'>No lessons yet!</p>}
+                                {props.courses ? props.courses?.map(x => <Card key={x.id} route={props.route} {...x} />) : props.lessons ? null : <p className='text-center'>No courses yet!</p>}
                             </div>
-
-                            <div className='small-screen'>
-                                {props.lessons ? props.lessons?.map(x => <Card key={x.id} route={props.route} {...x} />) : props.lessons ? null : <p className='text-center'>No lessons yet!</p>}
-                            </div>
-
-                            {props.courses ? props.courses?.map(x => <Card key={x.id} route={props.route} {...x} />) : props.lessons ? null : <p className='text-center'>No courses yet!</p>}
                         </div>
-                    </div>
-                }
+                    }
+                </div>
+
+                <div className='big-screen'>
+                    {props.isLoadingLessons || props.isLoadingCourses
+                        ? <BookSpinner />
+                        : <div style={{ paddingBottom: "2rem" }} className=''>
+                            <div className="row gy-5">
+                                <div className='big-screen'>
+                                    {props.lessons
+                                        ?
+                                        <div
+                                            id="carouselExampleControls"
+                                            className="carousel slide"
+                                            data-bs-ride="carousel"
+                                        >
+                                            <div className="carousel-inner">
+                                                <div className="carousel-item active" >
+                                                    <div style={{ display: 'flex' }}>
+                                                        <Card key={props.lessons[0].id} route={props.route} {...props.lessons[0]} />
+
+                                                        <Card key={props.lessons[0].id} route={props.route} {...props.lessons[0]} />
+                                                    </div>
+                                                </div>
+                                                <div className="carousel-item">
+                                                    <div style={{ display: 'flex' }}>
+
+                                                        {props.lessons?.map(x => <Card key={x.id} route={props.route} {...x} />)}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <button
+                                                className="carousel-control-prev"
+                                                type="button"
+                                                data-bs-target="#carouselExampleControls"
+                                                data-bs-slide="prev"
+                                            >
+                                                <span className="carousel-control-prev-icon" aria-hidden="true" />
+                                                <span className="visually-hidden">Previous</span>
+                                            </button>
+                                            <button
+                                                className="carousel-control-next"
+                                                type="button"
+                                                data-bs-target="#carouselExampleControls"
+                                                data-bs-slide="next"
+                                            >
+                                                <span className="carousel-control-next-icon" aria-hidden="true" />
+                                                <span className="visually-hidden">Next</span>
+                                            </button>
+                                        </div>
+                                        //props.lessons?.map(x => <Card key={x.id} route={props.route} {...x} />)
+                                        : props.courses ? null : <p className='text-center'>No lessons yet!</p>}
+                                </div>
+                                {props.courses ? props.courses?.map(x => <Card key={x.id} route={props.route} {...x} />) : props.lessons ? null : <p className='text-center'>No courses yet!</p>}
+                            </div>
+                        </div>
+                    }
+                </div>
             </div>
         </section>
     )
