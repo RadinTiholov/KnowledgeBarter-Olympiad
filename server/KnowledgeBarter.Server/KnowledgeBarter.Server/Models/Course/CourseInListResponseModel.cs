a276b9d80dd3e9817172b0fa0c04d11ed0d6d3d8
RadinTiholov <radin.tiholov@mail.bg>
2023-04-07 16:44:26 +0300

    Update CourseInList view model

--- server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Course/CourseInListResponseModel.cs
+++ server/KnowledgeBarter.Server/KnowledgeBarter.Server/Models/Course/CourseInListResponseModel.cs
@@ -16,6 +16,12 @@
 
         public string Owner { get; set; } = null!;
 
+        public string OwnerName { get; set; } = null!;
+
+        public int Likes { get; set; }
+
+        public int Comments { get; set; }
+
         public int[] Lessons { get; set; } = null!;
 
         public void CreateMappings(IProfileExpression configuration)
@@ -26,7 +32,13 @@
                 .ForMember(x => x.Lessons, opt =>
                     opt.MapFrom(i => i.Lessons.Select(x => x.Id).ToArray()))
                 .ForMember(x => x.Owner, opt =>
-                    opt.MapFrom(i => i.OwnerId));
+                    opt.MapFrom(i => i.OwnerId))
+                .ForMember(x => x.OwnerName, opt =>
+                    opt.MapFrom(i => i.Owner.UserName))
+                .ForMember(x => x.Likes, opt =>
+                    opt.MapFrom(i => i.Likes.Count))
+                .ForMember(x => x.Comments, opt =>
+                    opt.MapFrom(i => i.Lessons.Select(x => x.Comments.Count).Count()));
         }
     }
 }
