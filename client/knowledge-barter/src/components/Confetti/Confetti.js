5e28c9938c7a481a046efb3020ac0fcb3269a235
RadinTiholov <radin.tiholov@mail.bg>
2023-04-07 21:58:52 +0300

    Update menu UI

--- client/knowledge-barter/src/components/Confetti/Confetti.js
+++ client/knowledge-barter/src/components/Confetti/Confetti.js
@@ -1,21 +1,24 @@
 export const Confetti = () => {
-    document.getElementById("confetti-btn").addEventListener("click", function () {
-        var confetti = document.getElementById("confetti");
-        for (var i = 0; i < 50; i++) {
-            var dot = document.createElement("div");
-            dot.classList.add("confetti");
-            dot.style.left = Math.floor(Math.random() * confetti.offsetWidth) + "px";
-            dot.style.top = Math.floor(Math.random() * confetti.offsetHeight) + "px";
-            confetti.appendChild(dot);
+    const handleClick = () => {
+        const confetti = document.getElementById("confetti");
+        for (let i = 0; i < 50; i++) {
+          const dot = document.createElement("div");
+          dot.classList.add("confetti");
+          dot.style.left = Math.floor(Math.random() * confetti.offsetWidth) + "px";
+          dot.style.top = Math.floor(Math.random() * confetti.offsetHeight) + "px";
+          confetti.appendChild(dot);
         }
         setTimeout(function () {
-            confetti.innerHTML = "";
+          confetti.innerHTML = "";
         }, 3000);
-    });
+      }
+    
 
     return (
         <>
-            <button id="confetti-btn">Click me!</button>
+            <button id="confetti-btn" onClick={() => handleClick()}>
+                Confetti Button
+            </button>
             <div id="confetti"></div>
         </>
     );
--- client/knowledge-barter/src/components/Lessons/Lessons.js
+++ client/knowledge-barter/src/components/Lessons/Lessons.js
@@ -4,6 +4,7 @@ import { useContext, useEffect, useMemo, useState } from 'react'
 import { LessonContext } from '../../contexts/LessonContext'
 import { useSearchParams } from 'react-router-dom'
 import Pagination from '../common/Pagination/Pagination'
+import { Confetti } from '../Confetti/Confetti'
 
 let pageSize = 10;
 
@@ -40,37 +41,47 @@ export const Lessons = () => {
                     <h1 className="fw-bold mb-3 px-4 pt-5">All Lessons{searchParams.get('search') ? ` / ${searchParams.get('search')}` : ''}</h1>
                 </div>
             </div>
-
-            {/* Space */}
-            <div class="btn-group">
-                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Filter
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Option 1</a></li>
-                    <li><a class="dropdown-item" href="#">Option 2</a></li>
-                    <li><a class="dropdown-item" href="#">Option 3</a></li>
-                </ul>
+            <div className='container'>
+                <div className="card sort-menu">
+                    <div className="card-body">
+                        <div className='row'>
+                            <div className='col-sm-2'>
+                                <div className="btn-group">
+                                    <button className="btn sort-menu-button btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Sort by
+                                    </button>
+                                    <ul className="dropdown-menu">
+                                        <li><button className="dropdown-item" href="#">Likes</button></li>
+                                        <li><button className="dropdown-item" href="#">Views</button></li>
+                                        <li><button className="dropdown-item" href="#">Comments</button></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='col-sm-3'>
+                                <p>Filter by views <i className="fa-solid fa-eye fa-sm" /></p>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadio1000" />
+                                    <label className="form-check-label" for="flexRadio1000">
+                                        <p>1000 & up</p>
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadio100" />
+                                    <label className="form-check-label" for="flexRadio100">
+                                        <p>100 - 999</p>
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadio1" />
+                                    <label className="form-check-label" for="flexRadio1">
+                                        <p>1 - 99</p>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            {/* Space */}
-
-            <button id="confetti-btn">Click me!</button>
-            <div id="confetti"></div>
-
-
-            <div class="btn-group">
-                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Sort
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Option 1</a></li>
-                    <li><a class="dropdown-item" href="#">Option 2</a></li>
-                    <li><a class="dropdown-item" href="#">Option 3</a></li>
-                </ul>
-            </div>
-            {/* Space */}
-
-
 
             <div className="container">
                 <div className="text-center">
