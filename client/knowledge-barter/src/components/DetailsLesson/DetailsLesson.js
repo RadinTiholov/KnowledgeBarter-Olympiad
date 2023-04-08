badd374f4b8ce05f8bf749910f58085bc22b6756
RadinTiholov <radin.tiholov@mail.bg>
2023-04-08 12:34:08 +0300

    Add lesson state updating

--- client/knowledge-barter/src/components/DetailsLesson/DetailsLesson.js
+++ client/knowledge-barter/src/components/DetailsLesson/DetailsLesson.js
@@ -20,7 +20,7 @@ export const DetailsLesson = () => {
     const [isBought, isLoadingBoughtLeson] = useBoughtLesson(id);
     const [fullUserInfo, setfullUserInfo] = useCurrentUserInfo({});
     const [isLiked, setIsLiked] = useIsLiked(id, true);
-    const { delLesson } = useContext(LessonContext);
+    const { delLesson, like} = useContext(LessonContext);
     const { auth, updatePoints } = useContext(AuthContext);
     const [recommendedLessons, setRecommendedLessons] = useState([]);
 
@@ -57,6 +57,7 @@ export const DetailsLesson = () => {
     const likeLessonOnClick = () => {
         lessonService.like(id)
             .then(res => {
+                like(id)
                 setIsLiked(true);
                 setLesson(state => {
                     let temp = { ...state }
--- client/knowledge-barter/src/contexts/LessonContext.js
+++ client/knowledge-barter/src/contexts/LessonContext.js
@@ -5,25 +5,37 @@ export const LessonContext = createContext();
 
 export const LessonProvider = ({children}) => {
     const [lessons, setLessons] = useState([]);
+
     useEffect(() => {
         lessonsService.getAll()
             .then(res => setLessons(res))
             .catch(err => alert(err))
     }, [])
+
     const create = (lesson) => {
         setLessons(state => [...state, lesson])
-    }
+    };
+
     const update = (lesson) => {
         setLessons(state => lessons.map(x => x.id === lesson.id ? lesson : x));
-    }
+    };
+
     const delLesson = (id) => {
         setLessons(state => lessons.filter(x => x.id != id));
-    }
+    };
+
     const select = (id) => {
         return lessons.find(x => x.id == id) || {};
     };
+
+    const like = (id) => {
+        setLessons(state => lessons.map(x => x.id == id 
+            ? {...x, likes: x.likes + 1}
+            : x));
+    };
+
     return (
-        <LessonContext.Provider value={{lessons, create, update, delLesson, select}}>
+        <LessonContext.Provider value={{lessons, create, update, delLesson, select, like}}>
             {children}
         </LessonContext.Provider>  
     );
