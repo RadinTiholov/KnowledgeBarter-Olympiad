6ea1c7b121cfe300d6ba8305bf58a77e2a0217ec
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-04-17 13:28:21 +0300

    Add home page flip card

--- client/knowledge-barter/src/components/Home/PointsBanner/PointsBanner.js
+++ client/knowledge-barter/src/components/Home/PointsBanner/PointsBanner.js
@@ -1,9 +1,12 @@
 import './PointsBanner.css'
 import wave from '../../../images/wave2.png'
 import coinImage from '../../../images/coin-image.webp'
+import { Link } from 'react-router-dom'
 export const PointsBanner = () => {
     return (
-        <section id="points-banner">
+        <>
+
+            <section id="points-banner">
                 <img className="wave-image" src={wave} alt="wave" />
                 <h1 className="fw-bold text-xl-center">KBPoints</h1>
                 <div className="row" style={{ margin: "auto" }}>
@@ -16,18 +19,51 @@ export const PointsBanner = () => {
                         />
                     </div>
                     <div className="col-6 ">
-                        <p style={{ fontSize: "200%"}}>
-                            With KBP you can unlock lessons and courses.
-                        </p>
-                        <p style={{ fontSize: "200%"}}>
-                            They are your key to the unlimited knowledge.
-                        </p>
-                        <p style={{ fontSize: "200%" }}>
-                            You can earn some by creating content.
-                        </p>
+                        <div class="flip-container">
+                            <div class="flip-card">
+                                <div class="front">
+                                    <ul>
+                                        <li><p style={{ fontSize: "200%" }}>
+                                            With KBP you can unlock lessons and courses.
+                                        </p></li>
+                                        <li><p style={{ fontSize: "200%" }}>
+                                            They are your key to the unlimited knowledge.
+                                        </p></li>
+                                        <li><p style={{ fontSize: "200%" }}>
+                                            You can earn some by creating content.
+                                        </p></li>
+                                    </ul>
+                                </div>
+                                <div class="back">
+                                    <p style={{ fontSize: "200%" }}>
+                                        There are two ways to earn them:
+                                    </p>
+                                    <ul>
+                                        <li><p style={{ fontSize: "150%" }}>Creating a lesson which result in 100 credits given</p></li>
+                                        <li><p style={{ fontSize: "150%" }}>Creating a course which result in 500 credits given</p></li>
+                                    </ul>
+
+                                    <p style={{ fontSize: "200%" }}>
+                                        You only need to have a <Link to='/register'>registration</Link> on the platform to start!
+                                    </p>
+
+                                    {/* <Link
+                                            className="btn btn-outline-warning"
+                                            style={{ backgroundColor: "#636EA7" }}
+                                            to="/register"
+                                        >
+                                            Register
+                                        </Link> */}
+                                </div>
+                            </div>
+                        </div>
+
+
                         <p />
                     </div>
                 </div>
             </section>
+        </>
+
     )
 }
